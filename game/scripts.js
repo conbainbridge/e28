0bc28e70816f2bb334f1c7fdee6884135a7b0052
conbainbridge <cbainbridge@g.harvard.edu>
2019-09-18 16:04:54 -0400

    Added game for Assignment 2

---
+++ game/scripts.js
@@ -0,0 +1,24 @@
+let computerHand = ["rock", "paper", "scissors"];
+let button = document.getElementById('shoot');
+let gameResult = document.getElementById('game-result');
+button.addEventListener('click', gamePlay);
+function gamePlay() {
+    let humanHand = document.querySelector('input[name="hand"]:checked').value;
+    let i = Math.floor(Math.random() * 3);
+    let response = 'The computer chose ' + computerHand[i] + '.';
+    if (computerHand[i] == 'rock' && humanHand == 'paper') {
+        gameResult.innerHTML = response + ' <span class="win">You win this round!</span>';
+    } else if (computerHand[i] == 'rock' && humanHand == 'scissors') {
+        gameResult.innerHTML = response + ' <span class="lose">You lost this round.</span>';
+    } else if (computerHand[i] == 'paper' && humanHand == 'rock') {
+        gameResult.innerHTML = response + ' <span class="lose">You lost this round.</span>';
+    } else if (computerHand[i] == 'paper' && humanHand == 'scissors') {
+        gameResult.innerHTML = response + ' <span class="win">You win this round!</span>';
+    } else if (computerHand[i] == 'scissors' && humanHand == 'paper') {
+        gameResult.innerHTML = response + ' <span class="lose">You lost this round.</span>';
+    } else if (computerHand[i] == 'scissors' && humanHand == 'rock') {
+        gameResult.innerHTML = response + ' <span class="win">You win this round!</span>';
+    } else if (computerHand[i] == humanHand) {
+        gameResult.innerHTML = response + ' <span class="draw">That\'s a draw! Try again!</span>';
+    }
+}
