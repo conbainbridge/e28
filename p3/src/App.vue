1a13c186ca69a05eba31ca3f097a23fca5129134
Connie <37002158+conbainbridge@users.noreply.github.com>
2019-11-05 22:59:16 -0500

    initialized project 3 via Vue CLI

---
+++ p3/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <img alt="LiStack logo" src="./assets/logo.png" />
+    <p>LiStack - organizing life into to-do lists and notes</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  components: {}
+};
+</script>
+
+<style>
+</style>
