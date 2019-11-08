2de77bbac94996fc950ab02d7a73e7c9530e1d01
Connie <37002158+conbainbridge@users.noreply.github.com>
2019-11-07 21:55:09 -0500

    initial zipfoods commit - set up components

--- p3/src/components/NoteSlip.vue
+++ p3/src/components/NoteSlip.vue
@@ -1,10 +1,8 @@
 <template>
   <div>
     <div v-for="tester in testers" :key="tester.title" :title="tester.title" class="slip-style">
-      <div class="slip-title">
-        {{ tester.title }}
-        <button class="delete-button">X</button>
-      </div>
+      <div class="slip-title">{{ tester.title }}</div>
+      <button class="delete-button">X</button>
       <hr />
       <div class="slip-notes">{{ tester.notes }}</div>
     </div>
@@ -19,16 +17,18 @@ export default {
       testers: [
         {
           title: "Note #1",
+          id: "note1",
           notes: "These are the notes."
         },
         {
           title: "Note #2",
+          id: "note2",
           notes: "Oh look, even more notes! But what do they mean?"
         }
       ]
     };
   },
-  props: ["notes", "title"]
+  props: ["title", "notes"]
 };
 </script>
 
